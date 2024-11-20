a489c97bdbfe40b900a54e21a5f1eaca60ce2710
iaguilar-dt <irving@datatensor.com>
2024-11-20 15:50:54 -0600

    initial commit

---
+++ swipe-list/helpers/constants.ts
@@ -0,0 +1,7 @@
+export const list_data = [
+    {label: "Condition", value: "Good", key: "condition"},
+    {label: "Mileage", value: "20", key: "mileage"},
+    {label: "Color", value: "yellow", key: "color"},
+    {label: "Brand", value: "Volkswagen", key: "brand"},
+    {label: "Model", value: "beetle", key: "model"},
+];
